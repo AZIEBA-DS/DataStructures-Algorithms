6e427cb6e62e2bd4ec97624307600851b02963fc
AZIEBA-DS <70859945+AZIEBA-DS@users.noreply.github.com>
2020-09-08 13:55:05 -0400

    Add files via upload

---
+++ Inheritance/Diff.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Diff extends Sum {
+	
+	public Diff(int op1, int op2) {
+		super(op1, op2);
+	}
+	
+	public int operate() {
+		return this.getOperand1() - this.getOperand2();
+	}
+}
---
+++ Inheritance/IBinaryOperator.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+public class IBinaryOperator {
+
+	public void setOperand1(int o1) {
+		getOperand1();
+	}
+	public void setOperand2(int o2) {
+		getOperand2();
+	}
+	
+	public int getOperand1() {
+		return this.getOperand1();
+	}
+	public int getOperand2() {
+		return this.getOperand2();
+	}
+	
+	public int operate(){
+		return operate();
+	}
+}
