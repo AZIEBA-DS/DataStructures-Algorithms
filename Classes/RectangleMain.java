4b615aba27f18550dc59731430a066551dc3cac4
AZIEBA-DS <70859945+AZIEBA-DS@users.noreply.github.com>
2020-09-08 14:29:16 -0400

    Add files via upload

---
+++ Classes/RectangleMain.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class RectangleMain {
+	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle(4, 6);
+		
+		System.out.println("Width: " + rectangle.getWidth());
+		System.out.println("Height: " + rectangle.getHeight());
+	}
+}
